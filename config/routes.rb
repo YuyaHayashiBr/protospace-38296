5689eec5756180b7e4ac061bac9a248799116690
YuyaHayashiBr <yuya.hayashi.br@gmail.com>
2022-08-12 18:12:29 +0900

    commentsコントローラの作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root "prototypes#index"
   resources :prototypes, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+  resources :comments,   only: [:create]
 end
