e20deb0ad103145748749a17b9193699220448b6
YuyaHayashiBr <yuya.hayashi.br@gmail.com>
2022-08-12 18:07:55 +0900

    Commentモデルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :prototype
+  has_many :comment, dependent: :destroy
 
   validates :name,       presence: true
   validates :profile,    presence: true
