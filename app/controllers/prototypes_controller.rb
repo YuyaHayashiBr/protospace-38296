109b8cc757344e33bc3d8cf6f19705126aba98e6
YuyaHayashiBr <yuya.hayashi.br@gmail.com>
2022-08-12 17:53:04 +0900

    プロトタイプ削除機能を追加

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -35,6 +35,12 @@ class PrototypesController < ApplicationController
   end
 
   def destroy
+    prototype = Prototype.find(params[:id])
+    if prototype.destroy
+      redirect_to root_path
+    else
+      render :show
+    end
   end
 
   private
